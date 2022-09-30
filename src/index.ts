c0e471f585d29b94dd3d36fe0e5bee3df39c6478
Elis Odenhage <teknikgillaren@gmail.com>
2022-09-30 11:31:43 +0200

    added login

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,49 @@
 // import some modules that are required for the application
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+import bodyParser from "body-parser"
+import cors from "cors"
 
 // initiate express
 const app = express();
 
+// create a prisma client
+const prisma = new PrismaClient();
+
 // the entrt point
-const main = async() => {
+const main = async () => {
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(bodyParser.json())
+  app.use(cors())
+
   app.get("/", (req, res) => {
     res.end("Hello")
   });
 
+  app.post("/login", async (req, res) =>{
+    console.log(req.body)
+    const user = await prisma.user.findFirst({
+      where: {
+	username: req.body.username,
+	password: req.body.password
+      }}
+    )
+    if(user !== null){
+      res.end("Success");
+    }else{
+      res.end("Nope");
+    };
+  })
+
   app.listen(3001)
 }
 
-main();
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.log(e);
+    prisma.$disconnect();
+    process.exit(1);
+  });
