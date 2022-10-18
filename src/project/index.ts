b9e36c148badefd3ebf999cba9a226c74b568a9b
Elis Odenhage <teknikgillaren@gmail.com>
2022-10-18 12:05:24 +0200

    mutiple files, express router, file structure

---
+++ src/project/index.ts
@@ -0,0 +1,14 @@
+import express from "express"
+import prisma from "../clients/prisma"
+import createRouter from "./create"
+import authenticateToken from "../helpers/middleWare/authenticateToken"
+const router = express.Router()
+
+router.get('/list', helpers.middleWare.authenticateToken, async (req: any, res: any) => {
+  const projects = await prisma.project.findMany();
+  res.json(projects.filter((project: any) => project.author == req.user.name))
+})
+
+router.use('/create', createRouter)
+
+export default router;
